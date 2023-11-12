4db357453107cbc34d13ad28dbc1a154e57e918d
Vademandr <140320640+Vademandr@users.noreply.github.com>
2023-11-12 23:35:20 +0200

    create2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget.elements;
+
+    for (let i = 0; i < amount.value; i++) {
+      let position = i + 1;
+      const delays = Number(delay.value) + step.value * i;
+
+      createPromise(position, delays)
+        .then(({ position, delay }) => {
+          iziToast.show({
+            message: `✅ Fulfilled promise ${position} in ${delay}ms`,
+            position: 'topRight',
+            color: '#00ff00',
+          });
+        })
+        .catch(({ position, delay }) => {
+          iziToast.show({
+            message: `❌ Rejected promise ${position} in ${delay}ms`,
+            position: 'topRight',
+            color: '#ff0000',
+          });
+        }); 
   }
+
+  event.currentTarget.reset();
 }
